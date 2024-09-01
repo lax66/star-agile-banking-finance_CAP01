d4c4edea44331f69a14bac0aa4eee3214918fa85
Laxmikanta Giri <90712743+lax66@users.noreply.github.com>
2024-09-01 17:13:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+resource "aws_key_pair" "example" {
+  key_name = "key02"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name = "key02"
+
+  tags = {
+    Name = var.instance_name
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "cat /etc/os-release",
+      "mkdir -p /home/ubuntu/.ssh",
+      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
+      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
+      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
+    ]
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_key)
+   }
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
+  }
+  provisioner "local-exec" {
+        command = "ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible01.yml"
+  }
+}
