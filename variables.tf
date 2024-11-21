448223e4d6d2375a3b27304c8bda60bd7bca2951
Laxmikanta Giri <90712743+lax66@users.noreply.github.com>
2024-11-22 00:03:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,5 +20,5 @@ variable "ssh_private_key" {
 }
 variable "ssh_public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8sAMf6gNWEHRWfBZIemM2GqUc46MeaH64LZAbTT9ad"
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrXk5PS8pMNjEEmXhGIJMBSyI1wnhNrheAXVB9rvVYv"
 }
